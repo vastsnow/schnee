e831a0ad9476291011d2a2647c01e7e9454bfe6c
vastsnow <vastsnow@gmail.com>
2014-03-30 13:10:22 +0800

    const pointer

--- LearningTest/LearningTest.cpp
+++ LearningTest/LearningTest.cpp
@@ -3,13 +3,23 @@
 
 #include "stdafx.h"
 #include <iostream>
-
+using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	double dval=3.14;
-	const int &ri=dval;
-	std::cout<<ri<<std::endl;
-	const int temp=dval;
+	double pi=3.14;
+	double pi2=3.24;
+	const double *cptr=&pi;
+	double *const pptr=&pi;
+	const double *const pptt=&pi;
+	cptr=&pi2;
+	*pptr=3.15;
+//	*pptt=3;
+//	pptt=&pi2;
+	cout<<*cptr<<endl;
+	cout<<*pptr<<endl;
+	cout<<pi<<endl;
+	int *p1, *const p2;
+
 	system("pause");
 	return 0;
 }
