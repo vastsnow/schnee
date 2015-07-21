ddce5df6042950e288fab1e50dbf4e72aff85679
vastsnow <vastsnow@gmail.com>
2015-07-21 20:48:23 +0800

    Hello World

--- LearningTest/LearningTest.cpp
+++ LearningTest/LearningTest.cpp
@@ -6,20 +6,8 @@
 using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	double pi=3.14;
-	double pi2=3.24;
-	const double *cptr=&pi;
-	double *const pptr=&pi;
-	const double *const pptt=&pi;
-	cptr=&pi2;
-	*pptr=3.15;
-//	*pptt=3;
-//	pptt=&pi2;
-	cout<<*cptr<<endl;
-	cout<<*pptr<<endl;
-	cout<<pi<<endl;
-	int *p1, *const p2;
-
+	
+	cout << "Hello World!" << endl;
 	system("pause");
 	return 0;
 }
